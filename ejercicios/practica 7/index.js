a48c8c46a5bddf3c56a5abe88b0ec018ac7d69c2
Sulaiman256 <123740914+Sulaiman256@users.noreply.github.com>
2024-10-05 19:31:50 +0200

    Hecho la practica 7

--- ejercicios/practica 7/index.js
+++ ejercicios/practica 7/index.js
@@ -1,48 +1,80 @@
 console.log("Practica 7");
 
-// Practica 7 Atrapame si puedes
+let posicionActualJugador1 = 1;
 
-// Declaramemos dos variables que son los dos jugadores
-
-let posicionActualJugador = 1;
 let posicionActualJugador2 = 1;
 
 function movimientoJugador() {
-  // Ahora haremos que atraves del boton saque un numero aleatorio de 1 a 6 y esas seran las casillas que se movera
   document.querySelector("#jugador1").addEventListener("click", () => {
     let movimientoRandom = Math.floor(Math.random() * 6) + 1;
-    posicionActualJugador += movimientoRandom;
+    let posicionAnteriorJugador1 = posicionActualJugador1;
+    posicionActualJugador1 += movimientoRandom;
 
-    // Ahora haremos que cuando llegue a la ultima casilla, vuelva a la primera
-    if (posicionActualJugador > 20) {
-      posicionActualJugador = 20;
+    if (posicionActualJugador1 > 20) {
+      posicionActualJugador1 = 20;
     }
-    moverFicha("jugador1", posicionActualJugador);
-    alert("Te has movido" + movimientoRandom);
+    moverFicha("jugador1", posicionAnteriorJugador1, posicionActualJugador1);
+    alert("Te has movido " + movimientoRandom);
+    checkFichaActual();
+    checkEndGame();
   });
   document.querySelector("#jugador2").addEventListener("click", () => {
     let movimientoRandom = Math.floor(Math.random() * 6) + 1;
+    let posicionAnteriorJugador2 = posicionActualJugador2;
     posicionActualJugador2 += movimientoRandom;
 
-    // Ahora haremos que cuando llegue a la ultima casilla, vuelva a la primera
     if (posicionActualJugador2 > 20) {
       posicionActualJugador2 = 20;
     }
-    moverFicha("jugador2", posicionActualJugador2);
-    alert("Te has movido" + movimientoRandom);
+    moverFicha("jugador2", posicionAnteriorJugador2, posicionActualJugador2);
+    alert("Te has movido " + movimientoRandom);
+    checkFichaActual();
+    checkEndGame();
   });
 }
 
-// Ahora haremos una funcion que sea para mover fichas
+function moverFicha(jugador, posicionAnterior, posicionActual) {
+  const casillaAnterior = document.querySelector(
+    `#c${jugador === "jugador1" ? posicionAnterior : `${posicionAnterior}-2`}`
+  );
+  casillaAnterior.innerHTML = "";
 
-function moverFicha(jugador, posicion) {
-  const casillaJugador = document.querySelector(`${jugador}`);
-  const casillas = document.querySelectorAll(".casillas");
-  casillas.forEach((casilla) => {
-    casilla.innerHTML = "";
-  });
+  const nuevaCasilla = document.querySelector(
+    `#c${posicionActual}${jugador === "jugador1" ? "" : "-2"}`
+  );
+  const fichaActual = document.createElement("div");
 
-  const nuevaCasilla = document.querySelector(`c${posicion}`);
-  nuevaCasilla.appendChild(casillaJugador).cloneNode(true);
+  fichaActual.className =
+    jugador === "jugador1" ? "ficha jugador1" : "ficha jugador2";
+
+  nuevaCasilla.appendChild(fichaActual);
 }
+
+function checkFichaActual() {
+  // Hay que hacer que si ambos coinciden en el mismo numero de casilla la ultima en llegar volvera a la casilla 1
+  if (posicionActualJugador1 === posicionActualJugador2) {
+    const lastArrived =
+      posicionActualJugador1 > posicionActualJugador2 ? "jugador2" : "jugador1";
+
+    if (lastArrived === "jugador1") {
+      moverFicha("jugador1", posicionActualJugador1, 1);
+      posicionActualJugador1 = 1;
+    } else {
+      moverFicha("jugador2", posicionActualJugador2, 1);
+      posicionActualJugador2 = 1;
+    }
+    alert(lastArrived + " ha vuelto a la casilla de salida!!!");
+  }
+}
+// Ahora hay que hacer otra funcion que diga que cuando se llega a la casilla 20 se acabe el juego
+
+function checkEndGame() {
+  if (posicionActualJugador1 === 20 || posicionActualJugador2 === 20) {
+    alert("El juego ha terminado!");
+    alert("Reiniciando partida");
+    // hay que hacer que se cargue la pagina
+    window.location.reload();
+  }
+}
+
 movimientoJugador();
